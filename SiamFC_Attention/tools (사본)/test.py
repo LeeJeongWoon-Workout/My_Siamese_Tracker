fce2871767c2af8a63ec7485ad9064fec13b96f3
Tropical Penguin <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2022-05-18 12:47:17 +0900

    Add files via upload

---
+++ SiamFC_Attention/tools (사본)/test.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+import os
+from got10k.experiments import *
+from siamfc import TrackerSiamFC
+
+
+if __name__ == '__main__':
+    net_path='/home/airlab/PycharmProjects/pythonProject5/Siam-DW/pretrained/attention_pretrained1.pth'
+    tracker = TrackerSiamFC(net_path=net_path)
+
+    root_dir='/home/airlab/PycharmProjects/pythonProject5/data/LaSOTBenchmark'
+    e =ExperimentLaSOT(root_dir=root_dir)
+    e.run(tracker,visualize=False)
+    e.report([tracker.name])
