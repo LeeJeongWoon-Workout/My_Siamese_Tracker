4fb4e560e6082f7343196cae8b823956e11cab7c
Tropical Penguin <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2022-05-26 15:48:15 +0900

    Add files via upload

---
+++ Entropy_tracker/siamfc/utils.py
@@ -0,0 +1,126 @@
+import torch
+import numpy as np
+def load_pretrain(model, pretrained_path):
+    print('load pretrained model from {}'.format(pretrained_path))
+
+    device = torch.cuda.current_device()
+    pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+def remove_prefix(state_dict, prefix):
+    '''
+    Old style model is stored with all names of parameters share common prefix 'module.'
+    '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+
+    print('missing keys:{}'.format(missing_keys))
+    print('unused checkpoint keys:{}'.format(unused_pretrained_keys))
+    # print('used keys:{}'.format(used_pretrained_keys))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def rect_area(rects1,rects2,bound=None):
+    r"""Intersection over union.
+    Args:
+        rects1 (numpy.ndarray): An N x 4 numpy array, each line represent a rectangle
+            (left, top, width, height).
+        rects2 (numpy.ndarray): An N x 4 numpy array, each line represent a rectangle
+            (left, top, width, height).
+        bound (numpy.ndarray): A 4 dimensional array, denotes the bound
+            (min_left, min_top, max_width, max_height) for ``rects1`` and ``rects2``.
+    """
+    assert rects1.shape == rects2.shape
+    if bound is not None:
+        # bounded rects1
+        rects1[:, 0] = np.clip(rects1[:, 0], 0, bound[0])
+        rects1[:, 1] = np.clip(rects1[:, 1], 0, bound[1])
+        rects1[:, 2] = np.clip(rects1[:, 2], 0, bound[0] - rects1[:, 0])
+        rects1[:, 3] = np.clip(rects1[:, 3], 0, bound[1] - rects1[:, 1])
+        # bounded rects2
+        rects2[:, 0] = np.clip(rects2[:, 0], 0, bound[0])
+        rects2[:, 1] = np.clip(rects2[:, 1], 0, bound[1])
+        rects2[:, 2] = np.clip(rects2[:, 2], 0, bound[0] - rects2[:, 0])
+        rects2[:, 3] = np.clip(rects2[:, 3], 0, bound[1] - rects2[:, 1])
+
+
+    areas1 = np.prod(rects1[..., 2:], axis=-1)
+    areas2 = np.prod(rects2[..., 2:], axis=-1)
+
+    if areas1>areas2:
+        return areas2/areas1
+    else:
+        return areas1/areas2
+
+
+def rect_iou(rects1, rects2, bound=None):
+    r"""Intersection over union.
+    Args:
+        rects1 (numpy.ndarray): An N x 4 numpy array, each line represent a rectangle
+            (left, top, width, height).
+        rects2 (numpy.ndarray): An N x 4 numpy array, each line represent a rectangle
+            (left, top, width, height).
+        bound (numpy.ndarray): A 4 dimensional array, denotes the bound
+            (min_left, min_top, max_width, max_height) for ``rects1`` and ``rects2``.
+    """
+    assert rects1.shape == rects2.shape
+    if bound is not None:
+        # bounded rects1
+        rects1[:, 0] = np.clip(rects1[:, 0], 0, bound[0])
+        rects1[:, 1] = np.clip(rects1[:, 1], 0, bound[1])
+        rects1[:, 2] = np.clip(rects1[:, 2], 0, bound[0] - rects1[:, 0])
+        rects1[:, 3] = np.clip(rects1[:, 3], 0, bound[1] - rects1[:, 1])
+        # bounded rects2
+        rects2[:, 0] = np.clip(rects2[:, 0], 0, bound[0])
+        rects2[:, 1] = np.clip(rects2[:, 1], 0, bound[1])
+        rects2[:, 2] = np.clip(rects2[:, 2], 0, bound[0] - rects2[:, 0])
+        rects2[:, 3] = np.clip(rects2[:, 3], 0, bound[1] - rects2[:, 1])
+
+    rects_inter = _intersection(rects1, rects2)
+    areas_inter = np.prod(rects_inter[..., 2:], axis=-1)
+
+    areas1 = np.prod(rects1[..., 2:], axis=-1)
+    areas2 = np.prod(rects2[..., 2:], axis=-1)
+    areas_union = areas1 + areas2 - areas_inter
+
+    eps = np.finfo(float).eps
+    ious = areas_inter / (areas_union + eps)
+    ious = np.clip(ious, 0.0, 1.0)
+
+    return ious
+
+def _intersection(rects1, rects2):
+    r"""Rectangle intersection.
+    Args:
+        rects1 (numpy.ndarray): An N x 4 numpy array, each line represent a rectangle
+            (left, top, width, height).
+        rects2 (numpy.ndarray): An N x 4 numpy array, each line represent a rectangle
+            (left, top, width, height).
+    """
+    assert rects1.shape == rects2.shape
+    x1 = np.maximum(rects1[..., 0], rects2[..., 0])
+    y1 = np.maximum(rects1[..., 1], rects2[..., 1])
+    x2 = np.minimum(rects1[..., 0] + rects1[..., 2],
+                    rects2[..., 0] + rects2[..., 2])
+    y2 = np.minimum(rects1[..., 1] + rects1[..., 3],
+                    rects2[..., 1] + rects2[..., 3])
+
+    w = np.maximum(x2 - x1, 0)
+    h = np.maximum(y2 - y1, 0)
+
+    return np.stack([x1, y1, w, h]).T
